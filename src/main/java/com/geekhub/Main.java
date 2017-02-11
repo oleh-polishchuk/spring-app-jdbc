17b9f8668cd1eca121f5aae6500266194b5c7798
Oleh Polishchuk <oleh.polishchuk.ck@gmail.com>
2017-02-11 17:35:04 +0200

    Create Spring application with Java-based configuration

---
+++ src/main/java/com/geekhub/Main.java
@@ -0,0 +1,14 @@
+package com.geekhub;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Car car = context.getBean(Car.class);
+        car.startCar();
+    }
+}
